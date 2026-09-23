365807652a28e8c476750ef0c4710833b0abac1f
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-23 09:53:00 +0100

    feat: create first React component with fun facts list

--- Basic react static page/src/main.jsx
+++ Basic react static page/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+function Element() {
+  return (
+    <>
+      <img width="40px" alt="react-logo" src="./src/assets/react.svg" />
+      <h1>Fun facts about react</h1>
+      <ul>
+        <li>Was first realised in 2013</li>
+        <li>Was originally created by Jordan Walk</li>
+        <li>Has over 100k stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+}
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <Element></Element>
   </StrictMode>,
-)
+);
